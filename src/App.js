ab24f32af09f2b0e7ccdef72d91015ceca1d6c05
Diwash-101 <76509157+Diwash-101@users.noreply.github.com>
2023-06-14 20:17:05 +0545

    updated the design

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
 import "./App.css";
-import MainText from "./js/MainText.js";
+import MainText from "./js/Typewriter.js";
 
 function App() {
   return (
-    <div>
-      <div className="App">
-        <div className="maindisc">
-          <div className="profilepic"></div>
-          <p className="maindescription">Diwash Dahal </p>
+    <div className="App">
+      <div className="profilepic"></div>
+      <div className="content">
+        <div className="ltext">Hi, I'm</div>
+
+        <div className="name">Diwash Dahal </div>
+
+        <div className="ltext">
+          I am a <p className="description">Developer </p>
         </div>
+
         <MainText />
-      </div>
-      <div className="footer">
-        <a
-          href="https://www.instagram.com/d_boss_101_/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <ion-icon name="logo-instagram"></ion-icon>
-        </a>
-        <a
-          href="https://github.com/Diwash-101"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <ion-icon name="logo-github"></ion-icon>
-        </a>
+        <div className="footer">
+          <a
+            href="https://www.instagram.com/d_boss_101_/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <ion-icon name="logo-instagram"></ion-icon>
+          </a>
+          <a
+            href="https://github.com/Diwash-101"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <ion-icon name="logo-github"></ion-icon>
+          </a>
+        </div>
       </div>
     </div>
   );
